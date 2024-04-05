e29f4f106c6e67cce31332c02d9a98631f7d9072
Saravanan0804 <it20215224@my.sliit.lk>
2024-04-05 11:42:05 +0530

    Add: CV and redux configuration

---
+++ my-cv/src/store/reducers/authReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    // Initial state properties
+};
+
+const authReducer = (state = initialState, action) => {
+    // Reducer logic based on action types
+    return state;
+};
+
+export default authReducer;
